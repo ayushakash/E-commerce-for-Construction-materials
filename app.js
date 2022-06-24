b5bce0749b5b04205d4bce09426089f6ee9ed502
Ayush <ayushakash9@gmail.com>
2022-06-24 14:55:02 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const app = express();
+const bodyParser = require("body-parser");
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/',(req,res)=>{
+
+    res.render('login');
+})
+
+app.listen(3000,()=>{
+
+    console.log('server running at port 3000');
+})
