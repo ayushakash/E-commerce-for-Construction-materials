faeaf4092403fd526d9ce623b5a6778929c7a387
ayushakash <ayushakash9@gmai.com>
2022-08-11 14:02:02 +0530

    updated .env

--- mongo.js
+++ mongo.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://chardeevari:chardeevari%40mongo@cluster0.rfwfs.mongodb.net/?retryWrites=true&w=majority',
-  {
+mongoose.connect(process.env.MONGO_URI,
+
+{
     useNewUrlParser: true,
     useUnifiedTopology: true
   }
